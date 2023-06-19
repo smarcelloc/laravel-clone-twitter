fba6373ee4151a885affad2c880ff1bd2f700255
Marcello Silvério <smarcelloc@gmail.com>
2023-06-19 02:09:12 +0000

    feat: create tweet

---
+++ app/Http/Livewire/Tweet/Create.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire\Tweet;
+
+use App\Models\Tweet;
+use Livewire\Component;
+
+class Create extends Component
+{
+    public ?string $body = null;
+
+    public function render()
+    {
+        return view('livewire.tweet.create');
+    }
+
+    public function submit() {
+        Tweet::query()->create([
+            'body' => $this->body,
+            'created_by' => auth()->id()
+        ]);
+
+        $this->emit('tweet::created');
+    }
+}
