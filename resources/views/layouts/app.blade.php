539363744bba032821121436a251d25e3cbb8641
Marcello Silvério <smarcelloc@gmail.com>
2023-06-19 03:25:48 +0000

    chore: add icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased bg-black text-gray-100">
-        <div class="min-h-screen">
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-        @livewireScripts
-    </body>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
+</head>
+
+<body class="font-sans antialiased bg-black text-slate-100">
+    <div class="container mx-auto min-h-screen grid grid-cols-4">
+        <nav class="block">
+
+        </nav>
+        <main class="block col-span-2 border-x-[0.5px] border-[#2f3336]"></main>
+        <aside class="block"></aside>
+    </div>
+    @livewireScripts
+</body>
+
 </html>
